6cc347130519e739b8c2af17904daf0f20ec0469
BradRice2202 <bradley.rice2202@gmail.com>
2023-10-11 17:30:47 +0200

    first commit

---
+++ test2.php
@@ -0,0 +1,19 @@
+<?php
+    $names = ["Bradley","Gavin","Nick","Alexis","Pierre","Tom","Aran","Luke","Allison","Martin","Caelan","Aston","Wielie","Cameron","Jeanette","Jude","Danny","Thomas","James","Jake"];
+    $surnames = ["Rice","Burns","Poon","Strouthos","Van De Merwe","Brooks","Groesbeek","Greenburg","Botha","Johston","Black","Miles","West","North","Ocean","Banks","Creed","Tracey","John","Cash"];
+
+    foreach ($names as $name)
+    {
+        foreach ($surnames as $surname)
+        {
+            $person = [];
+
+            $birthdate = 
+
+            $person = [$name + $surname];
+        };
+    };
+
+
+
+?>
