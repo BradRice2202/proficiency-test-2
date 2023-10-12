c83e74ad51751deba082591a7a81e6df62e3cbdf
BradRice2202 <bradley.rice2202@gmail.com>
2023-10-12 13:06:26 +0200

    second commit

--- test2.php
+++ test2.php
@@ -2,18 +2,44 @@
     $names = ["Bradley","Gavin","Nick","Alexis","Pierre","Tom","Aran","Luke","Allison","Martin","Caelan","Aston","Wielie","Cameron","Jeanette","Jude","Danny","Thomas","James","Jake"];
     $surnames = ["Rice","Burns","Poon","Strouthos","Van De Merwe","Brooks","Groesbeek","Greenburg","Botha","Johston","Black","Miles","West","North","Ocean","Banks","Creed","Tracey","John","Cash"];
 
+    // header("Content-type: text/csv");
+
+    // $filename = "people.csv";
+
+    // header("Content-Disposition: attachment; filename = $filename");
+
+    $output = fopen("php://output" , "w");
+
     foreach ($names as $name)
     {
         foreach ($surnames as $surname)
         {
             $person = [];
 
-            $birthdate = 
+            $date_string= mt_rand(1262055681,1262055681);
 
-            $person = [$name + $surname];
+            $birthdate = date("D-m-y", $date_string);
+
+            $person = [$name, $surname, substr($name, 0, 1), $birthdate];
+
+            echo'<pre>';
+            print_r($person);
+
+            // $header = array_keys($person[0]);
+
+            // fputcsv($output, $header);
+
+            // foreach($person as $row)
+            // {
+                // fputcsv($output, $row);
+            // };
+
+            // fclose($output);
+
         };
     };
 
+    
 
 
 ?>
