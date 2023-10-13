6db653436e5e14accd4218c5be29866e2161382c
BradRice2202 <bradley.rice2202@gmail.com>
2023-10-13 02:55:10 +0200

    3rd commit

--- test2.php
+++ test2.php
@@ -1,45 +1,141 @@
 <?php
+    $fields = ["Id","Name","Surname","Initials","Age","DateOfBirth"];
     $names = ["Bradley","Gavin","Nick","Alexis","Pierre","Tom","Aran","Luke","Allison","Martin","Caelan","Aston","Wielie","Cameron","Jeanette","Jude","Danny","Thomas","James","Jake"];
     $surnames = ["Rice","Burns","Poon","Strouthos","Van De Merwe","Brooks","Groesbeek","Greenburg","Botha","Johston","Black","Miles","West","North","Ocean","Banks","Creed","Tracey","John","Cash"];
+    $id = 1;
+    $test = 0;
+    $first = true;
 
-    // header("Content-type: text/csv");
+    $amountOfRecords = $_POST["amtRecords"];
 
-    // $filename = "people.csv";
-
-    // header("Content-Disposition: attachment; filename = $filename");
-
-    $output = fopen("php://output" , "w");
-
-    foreach ($names as $name)
-    {
-        foreach ($surnames as $surname)
+    function add_quotes($str)
         {
-            $person = [];
 
-            $date_string= mt_rand(1262055681,1262055681);
+            // $fileurl = "127.0.0.1/output/output.csv";
 
-            $birthdate = date("D-m-y", $date_string);
+            
+            // sprintf('"%s"',$str);
 
-            $person = [$name, $surname, substr($name, 0, 1), $birthdate];
+            // $url = "127.0.0.1/PROFICIENCY-TEST-2/output/output.csv";
+            // $ch = curl_init($url);
+            // $dir = './';
+            // $file_name = basename($url);
+            // $save_file_loc = $dir . $file_name;
+            // $fp = fopen($save_file_loc, 'wb');
+            // curl_setopt($ch, CURLOPT_FILE, $fp);
+            // curl_setopt($ch, CURLOPT_HEADER, 0);
+            // curl_exec($ch);
+            // curl_close($ch);
+            // fclose($fp);
 
-            echo'<pre>';
-            print_r($person);
+            // downloadCsv();
 
-            // $header = array_keys($person[0]);
+            // header("Content-disposition: attachment; filename=\'$fileName'");
 
-            // fputcsv($output, $header);
+            // header("Content-Disposition: attatchment; file_url=$fileurl");
 
-            // foreach($person as $row)
-            // {
-                // fputcsv($output, $row);
-            // };
-
-            // fclose($output);
+            return sprintf('"%s"',$str);
+            // sprintf('"%s"',$str);
 
         };
-    };
-
-    
 
 
-?>
+        while($id <= $amountOfRecords)
+        {
+            $lastName = $surnames;
+
+            foreach ($names as $name)
+            {
+                    
+                $currentTime = time();
+
+                $currentDate = gmdate("d-m-y", $currentTime);
+
+                $date_string= mt_rand(1262055681,$currentTime);
+
+                $birthdate = date("d-m-y", $date_string);
+
+                $birthYear = intval(date("y", $date_string));
+
+                $yearNow = intval(date("y", $currentTime));
+
+                $age = $yearNow - $birthYear;
+
+                $person = [];
+                       
+                $person = [$id,$name, $lastName[$test], substr($name, 0, 1),$age, $birthdate];
+
+                $finalArr = array();
+
+                array_push($finalArr,$person);
+
+                $id++;
+                $test++;
+
+                if($test <= 10)
+                {
+                    $test = 0;
+                };
+
+                if($amountOfRecords <= 1){
+                    array_unshift($finalArr, $fields);
+                    break;
+                };
+
+                $file = 'output/output.csv';
+
+                if($first)
+                {
+                    array_unshift($finalArr, $fields);
+                }
+                
+
+                foreach($finalArr as $d)
+                {                      
+                    $csv = implode(',', array_map("add_quotes",$d));
+                    file_put_contents($file,$csv."\r\n", FILE_APPEND);
+                    // $urlFile = "127.0.0.1:2000/PROFICIENCY-TEST-2/output/output.csv";
+                    // $urlFile = "output/output.csv";
+                    // $fileName = basename($urlFile);
+                    // $fn = file_put_contents($fileName,file_get_contents($urlFile));
+                    // header("Expires:0");
+                    // header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+                    // header("Cache-Control: no-store, no-cache, must-revalidate");
+                    // header("Cache-Control: post-check=0, pre-check=0", false);
+                    // header("Pragma: no-cache");
+                    // header("Content-type: text/csv");
+                    // header('Content-length: '.filesize($fileName));
+                    // header('Location: ' . $_SERVER['HTTP_REFERER']);
+                    // header('Content-disposition: attachment; filename="'.basename($fileName).'"');
+                    // readfile($fileName);
+                    // header('Location: ' . $_SERVER['HTTP_REFERER']);
+                    $first = false;
+                }
+            };
+             
+        };
+
+        $urlFile = "127.0.0.1:2000/PROFICIENCY-TEST-2/output/output.csv";
+        $urlFile = "output/output.csv";
+        $fileName = basename($urlFile);
+        $fn = file_put_contents($fileName,file_get_contents($urlFile));
+        header("Expires:0");
+        header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+        header("Cache-Control: no-store, no-cache, must-revalidate");
+        header("Cache-Control: post-check=0, pre-check=0", false);
+        header("Pragma: no-cache");
+        header("Content-type: text/csv");
+        header('Content-length: '.filesize($fileName));
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+        header('Content-disposition: attachment; filename="'.basename($fileName).'"');
+        readfile($fileName);
+
+        // $urlFile = "output/output.csv";
+        // $fileName = basename($urlFile);
+        // $fn = file_put_contents($fileName,file_get_contents($urlFile));
+        // header('Location: ' . $_SERVER['HTTP_REFERER']);
+        // header('Content-disposition: attachment; filename="'.basename($fileName).'"');
+        // readfile($fileName);
+
+
+        // header('Location: ' . $_SERVER['HTTP_REFERER']);
