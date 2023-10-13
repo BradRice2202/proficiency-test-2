e7038a007f66a76382ab4bdbffa2ed3dc1b52631
BradRice2202 <bradley.rice2202@gmail.com>
2023-10-13 17:00:36 +0200

    started adding all records into csv file and added ability to save file to computer

--- index.php
+++ index.php
@@ -1,58 +1,21 @@
 <?php
-    // $fields = ["Id","Name","Surname","Initials","Age","DateOfBirth"];
+    $fields = ["Id","Name","Surname","Initials","Age","DateOfBirth"];
     $names = ["Bradley","Gavin","Nick","Alexis","Pierre","Tom","Aran","Luke","Allison","Martin","Caelan","Aston","Wielie","Cameron","Jeanette","Jude","Danny","Thomas","James","Jake"];
     $surnames = ["Rice","Burns","Poon","Strouthos","Van De Merwe","Brooks","Groesbeek","Greenburg","Botha","Johston","Black","Miles","West","North","Ocean","Banks","Creed","Tracey","John","Cash"];
     $id = 1;
     $test = 0;
     $first = true;
 
-    // $amountOfRecords = $_POST["amtRecords"];
-
-    // if(array_key_exists('csv-file-from-browse', $_POST))
-    // {
-        $row = 1;
-        if(($handle = fopen("output.csv", "r")) !== FALSE)
-        {
-            while(($data = fgetcsv($handle, 100, ",")) !== FALSE)
-            {
-                $num = count($data);
-                echo "<p> $num fields in line $row: <br /></p>\n";
-                $row++;
-                for($c=1; $c < $num; $c++)
-                {
-                    echo $data[$c]."<br />\n";
-                };
-            };
-            fclose($handle);
-
-            exit;
-        };
-        // echo"<pre>";
-        // print_r($_FILES);
-        // exit;
-    // };
-
-    function read_csv($fileName, $length)
-    {
-
-        print_r($_FILES[0]);
-        exit();
-
-        $file = fopen($_FILES[$fileName]["tmp_name"], "r");
-
-        $headers = fgetcsv($file, $length,",");
-
-        while(($data = fgetcsv($file, $length,",")) !== FALSE)
-        {
-            print_r($data[1]);
-        };
-
-    };
+    $amountOfRecords = $_POST["amtRecords"];
 
     function add_quotes($str)
         {
+
+            $fileurl = "127.0.0.1/output/output.csv";
+
             return sprintf('"%s"',$str);
         };
+
 
         while($id <= $amountOfRecords)
         {
@@ -83,13 +46,12 @@
 
                 array_push($finalArr,$person);
 
-
                 $id++;
                 $test++;
 
-                while($test >= 20)
+                if($test <= 10)
                 {
-                    $test = 0;   
+                    $test = 0;
                 };
 
                 if($amountOfRecords <= 1){
@@ -109,28 +71,17 @@
                 {                      
                     $csv = implode(',', array_map("add_quotes",$d));
                     file_put_contents($file,$csv."\r\n", FILE_APPEND);
+                    $urlFile = "127.0.0.1:2000/output.csv";
+                    $urlFile = "/output.csv";
+                    $fileName = basename($urlFile);
+                    $fn = file_put_contents($fileName,file_get_contents($file));
+                    // header('Location: ' . $_SERVER['HTTP_REFERER']);
                     $first = false;
                 }
-
-                
             };
-            
              
         };
 
-        $urlFile = "127.0.0.1:2020/PROFICIENCY-TEST-2/output/output.csv";
-        $urlFile = "output/output.csv";
-        $fileName = basename($urlFile);
-        $fn = file_put_contents($fileName,file_get_contents($urlFile));
-        header("Expires:0");
-        header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
-        header("Cache-Control: no-store, no-cache, must-revalidate");
-        header("Cache-Control: post-check=0, pre-check=0", false);
-        header("Pragma: no-cache");
-        header("Content-type: text/csv");
-        header('Content-length: '.filesize($fileName));
-        header('Content-disposition: attachment; filename="'.basename($fileName).'"');
-        readfile($fileName);
         // header('Location: ' . $_SERVER['HTTP_REFERER']);
-
-    
+        header("Content-Disposition: attachment; filename=$fileName");
+        // readfile($fileName);
