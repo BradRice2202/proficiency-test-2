e438384387eaff1d5f325c90ba58020e887168d2
BradRice2202 <bradley.rice2202@gmail.com>
2023-10-15 14:05:07 +0200

    creates csv file and reads from uploaded csv file with no errors

--- index.php
+++ index.php
@@ -1,28 +1,17 @@
 <?php
+
+    session_start();
+
     // $fields = ["Id","Name","Surname","Initials","Age","DateOfBirth"];
     $names = ["Bradley","Gavin","Nick","Alexis","Pierre","Tom","Aran","Luke","Allison","Martin","Caelan","Aston","Wielie","Cameron","Jeanette","Jude","Danny","Thomas","James","Jake"];
     $surnames = ["Rice","Burns","Poon","Strouthos","Van De Merwe","Brooks","Groesbeek","Greenburg","Botha","Johston","Black","Miles","West","North","Ocean","Banks","Creed","Tracey","John","Cash"];
     $id = 1;
     $test = 0;
     // $first = true;
-
-        // echo"<pre>";
-        // print_r($_FILES);
-        // exit;
-
-    if(isset($_POST['amtRecords']))
-    {
-        $amountOfRecords = $_POST["amtRecords"];
-
-    }
     
-
         $row = 0;
-        if(($handle = fopen("127.0.0.1:2000/output.csv", "r")) !== FALSE) //<- WORKS BUT DOWNLOADS AN HTML FILE
-        // if($handle = file_get_contents($_FILES['uploadedCsv']['tmp_name']))
+        if(($handle = fopen("output.csv", "r")) !== FALSE)
         {
-            // $header('content_disposition: attachment; filename="output.csv"');
-            
             while(($data = fgetcsv($handle, 100, ",")) !== FALSE)
             {
                 $num = count($data);
@@ -45,73 +34,81 @@
             return sprintf('"%s"',$str);
         };
 
-
-        while($id <= $amountOfRecords)
+        if(isset($_POST['submitAmtRecords']))
         {
-            $lastName = $surnames;
+            $amountOfRecords = $_POST["amtRecords"];
 
-            foreach ($names as $name)
+            while($id <= $amountOfRecords)
+
             {
-                    
-                $currentTime = time();
-
-                $currentDate = gmdate("d-m-y", $currentTime);
-
-                $date_string= mt_rand(1262055681,$currentTime);
-
-                $birthdate = date("d-m-y", $date_string);
-
-                $birthYear = intval(date("y", $date_string));
-
-                $yearNow = intval(date("y", $currentTime));
-
-                $age = $yearNow - $birthYear;
-
-                $person = [];
-                       
-                $person = [$id,$name, $lastName[$test], substr($name, 0, 1),$age, $birthdate];
-
-                $finalArr = array();
-
-                array_push($finalArr,$person);
-
-                $id++;
-                $test++;
-
-                if($test >= 20)
+                $lastName = $surnames;
+    
+                foreach ($names as $name)
                 {
-                    $test = 0;
+                        
+                    $currentTime = time();
+    
+                    $currentDate = gmdate("d-m-y", $currentTime);
+    
+                    $date_string= mt_rand(1262055681,$currentTime);
+    
+                    $birthdate = date("d-m-y", $date_string);
+    
+                    $birthYear = intval(date("y", $date_string));
+    
+                    $yearNow = intval(date("y", $currentTime));
+    
+                    $age = $yearNow - $birthYear;
+    
+                    $person = [];
+                           
+                    $person = [$id,$name, $lastName[$test], substr($name, 0, 1),$age, $birthdate];
+    
+                    $finalArr = array();
+    
+                    array_push($finalArr,$person);
+    
+                    $id++;
+                    $test++;
+    
+                    if($test >= 20)
+                    {
+                        $test = 0;
+                    };
+    
+                    // if($amountOfRecords <= 1){
+                    //     array_unshift($finalArr, $fields);
+                    //     break;
+                    // };
+    
+                    $file = 'output/output.csv';
+    
+                    // if($first)
+                    // {
+                    //     array_unshift($finalArr, $fields);
+                    // }
+                    
+    
+                    foreach($finalArr as $d)
+                    {                      
+                        $csv = implode(',', array_map("add_quotes",$d));
+                        file_put_contents($file,$csv."\r\n", FILE_APPEND);
+                        $urlFile = "127.0.0.1:2000/output.csv";
+                        $urlFile = "/output.csv";
+                        $fileName = basename($urlFile);
+                        $fn = file_put_contents($fileName,file_get_contents($file));
+    
+                        header("Content-Disposition: attachment; filename=$fileName");
+                        // header('Location: ' . $_SERVER['HTTP_REFERER']);
+                        // $first = false;
+                    }
                 };
-
-                // if($amountOfRecords <= 1){
-                //     array_unshift($finalArr, $fields);
-                //     break;
-                // };
-
-                $file = 'output/output.csv';
-
-                // if($first)
-                // {
-                //     array_unshift($finalArr, $fields);
-                // }
-                
-
-                foreach($finalArr as $d)
-                {                      
-                    $csv = implode(',', array_map("add_quotes",$d));
-                    file_put_contents($file,$csv."\r\n", FILE_APPEND);
-                    $urlFile = "127.0.0.1:2000/output.csv";
-                    $urlFile = "/output.csv";
-                    $fileName = basename($urlFile);
-                    $fn = file_put_contents($fileName,file_get_contents($file));
-
-                    header("Content-Disposition: attachment; filename=$fileName");
-                    // header('Location: ' . $_SERVER['HTTP_REFERER']);
-                    // $first = false;
-                }
+                 
             };
-             
+            header("Content-Disposition: attachment; filename=$fileName");
         };
+
+        
 
         // header('Location: ' . $_SERVER['HTTP_REFERER']);
         // header("Content-Disposition: attachment; filename=$fileName");
